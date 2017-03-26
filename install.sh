5068da12942e6aec9bd2a135cf62664e92d0db1b
Amit Chaudhary <studenton.com@gmail.com>
2017-03-26 09:19:38 +0545

    Switch to one step installation

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-cp earthquake.py /bin/earthquake
+git clone http://github.com/studenton/QuakeCL.git
+cp QuakeCL/earthquake.py /bin/earthquake
 chmod a+x /bin/earthquake
 echo "Installation complete."
 echo "Type earthquake to start using it."
+sudo rm -rf QuakeCL
