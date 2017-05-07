57aa50302de85c15e21271ea939f9b206b662595
Amit Chaudhary <meamitkc@gmail.com>
2017-05-07 19:30:39 +0545

    Add unit tests

---
+++ test_earthquake.py
@@ -0,0 +1,18 @@
+import unittest
+import earthquake
+from datetime import date
+
+
+class Tests(unittest.TestCase):
+
+    def test_convert_to_date(self):
+        got = earthquake.convert_to_date('1995-07-10')
+        expected = date(1995, 7, 10)
+        self.assertEqual(got, expected)
+
+    def test_get_page(self):
+        result = earthquake.get_page('http://abcde.fghij')
+        self.assertFalse(result)
+
+if __name__ == '__main__':
+    unittest.main()
