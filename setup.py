0e070c5292f297317c81489e2425f0e254b05b18
Amit <meamitkc@gmail.com>
2017-10-20 21:44:08 +0545

    Update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requirements = [pkg.split('=')[0] for pkg in open('requirements.txt')]
 
 setup(
     name='QuakeCL',
-    version='0.1.6',
+    version='0.1.7',
     license='MIT',
     description='A command line tool that displays the recent earthquakes and aftershocks in Nepal.',
     long_description=open('README.md').read(),
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/amitness/QuakeCL',
     python_requires='>=3',
     install_requires=requirements,
-    download_url='https://github.com/amitness/QuakeCL/tarball/0.1.6',
+    download_url='https://github.com/amitness/QuakeCL/tarball/0.1.7',
     packages=['quakecl', ],
     test_suite='quakecl.tests',
     entry_points="""
