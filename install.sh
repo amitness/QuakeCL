828cedb4157165d23b29a8dc411643824bbeee78
Mark van Lent <mark@vlent.nl>
2017-10-20 15:12:37 +0200

    Remove unneeded install script.

--- install.sh
+++
@@ -1,6 +0,0 @@
-git clone http://github.com/amitness/QuakeCL.git --quiet
-cp QuakeCL/earthquake.py /bin/earthquake
-chmod a+x /bin/earthquake
-echo "Installation complete."
-echo "Type earthquake to start using it."
-sudo rm -rf QuakeCL
