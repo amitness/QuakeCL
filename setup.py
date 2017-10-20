68f5bb4b7e4bae04611f7331b9bb54ab419cb9a6
Amit <meamitkc@gmail.com>
2017-10-20 21:13:18 +0545

    Increase version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='QuakeCL',
-    version='0.1.1',
+    version='0.1.3',
     license='MIT',
     description='A command line tool that displays the recent earthquakes and aftershocks in Nepal.',
     long_description=open('README.md').read(),
@@ -16,7 +16,7 @@ setup(
     author_email='meamitkc@gmail.com',
     url='https://github.com/amitness/QuakeCL',
     python_requires='>=3',
-    download_url = 'https://github.com/amitness/QuakeCL/tarball/0.1.0',
+    download_url='https://github.com/amitness/QuakeCL/tarball/0.1.3',
     packages=['quakecl', ],
     test_suite='quakecl.tests',
     entry_points="""
