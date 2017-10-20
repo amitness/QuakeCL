f7ef5e76f80f6ef20c536bff100d221f22674f43
Amit Chaudhary <meamitkc@gmail.com>
2017-10-20 20:41:58 +0545

    Make version and tag version same

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='QuakeCL',
-    version='0.1.dev0',
+    version='0.1.0',
     license='MIT',
     description='A command line tool that displays the recent earthquakes and aftershocks in Nepal.',
     long_description=open('README.md').read(),
