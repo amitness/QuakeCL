73d09be0312d99851196db77acde5804f9082d40
Amit <meamitkc@gmail.com>
2017-10-20 22:36:35 +0545

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='QuakeCL',
-    version='0.2.0',
+    version='0.2.1',
     license='MIT',
     description='A command line tool that displays the recent earthquakes and aftershocks in Nepal.',
     long_description=open('README.md').read(),
@@ -15,7 +15,7 @@ setup(
     author_email='meamitkc@gmail.com',
     url='https://github.com/amitness/QuakeCL',
     install_requires=['requests'],
-    download_url='https://github.com/amitness/QuakeCL/tarball/0.2.0',
+    download_url='https://github.com/amitness/QuakeCL/tarball/0.2.1',
     packages=['quakecl', ],
     test_suite='quakecl.tests',
     entry_points="""
